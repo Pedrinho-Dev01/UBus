27f8b1109c2503b495e40424ea12117a8295ad38
Daniel Pedrinho <dpedrinho01@gmail.com>
2023-05-18 09:42:39 +0100

    Css

--- UBus-react/src/containers/search/search.jsx
+++ UBus-react/src/containers/search/search.jsx
@@ -17,18 +17,16 @@ const Search = ({onClose}) => {
   return (
     <div className="modal-overlay K7__header section__padding">
       <div className="modal-content K7__header-content">
+      
+      <button className="close-button" onClick={handleClose}>
+          X
+      </button>
+
         <h2 className='gradient__text'>Search for More Information</h2>
+        <p></p>
         <input id="search-input" type="text" placeholder="Enter your search query" />
-        <button type="button" onClick={handleSearch}>
+        <button type="button" className='searchBtn' onClick={handleSearch}>
           Search
-        </button>
-
-        <div className="additional-content K7__header-content__input">
-          <p>Additional information can go here.</p>
-        </div>
-
-        <button className="close-button" onClick={handleClose}>
-          Close
         </button>
       </div>
     </div>
