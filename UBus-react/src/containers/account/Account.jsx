44512170b2ccda7744eec096c3df5c4cb4ae3639
joaoamrodrigues <joaoamrodrigues@ua.pt>
2023-05-21 19:04:57 +0100

    Account

--- UBus-react/src/containers/account/Account.jsx
+++ UBus-react/src/containers/account/Account.jsx
@@ -1,11 +1,86 @@
 import React from 'react';
 import './account.css';
+import steering_wheel from '../../assets/steering_wheel.svg'
+import wallet from '../../assets/wallet.svg'
+import gift from '../../assets/gift.svg'
+import letter from '../../assets/letter.svg'
+import discount from '../../assets/discount.svg'
+import settings from '../../assets/settings.svg'
 
 const Account = () => {
   return (
     <div>
       <h1 id='account-section' className='section-title'>Account</h1>
+      <h2 id='account_id'></h2>
+      <div class="table">
+        <table border={1} class="center">
+          <tr>
+          <td>
+          <img src={steering_wheel} style={{width: "50px"}}/>
+          </td>
+          <td>
+            <b>Driver Mode</b>
+          </td>
+          <td>
+            <label class="switch">
+              <input type="checkbox" checked></input>
+              <span class="slider round"></span>
+            </label>
+          </td>
+          </tr>
+          <tr>
+            <td>
+              <img src={wallet} style={{width: "50px"}}/>
+            </td>
+            <td>
+              <b>Balance</b>
+            </td>
+            <td>
+              <b>€0,00</b>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img src={letter} style={{width: "50px"}}/>
+            </td>
+            <td colspan="2">
+              <b>Messages</b>
+            </td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>
+              <img src={gift} style={{width: "50px"}}/>
+            </td>
+            <td colspan="2">
+              <b>Send a gift</b>
+            </td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>
+              <img src={discount} style={{width: "50px"}}/>
+            </td>
+            <td colspan="2">
+              <b>Promotions</b>
+            </td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>
+              <img src={settings} style={{width: "50px"}}/>
+            </td>
+            <td colspan="2">
+              <b>Settings</b>
+            </td>
+          </tr>
+        </table>
+      </div>
     </div>
+
+    
+    
+
   )
 }
 
