5e015ef7c5f0b54b972580ff406b09e9d4edf486
Daniel Pedrinho <dpedrinho01@gmail.com>
2023-05-24 19:12:02 +0100

    Unecessary Delete

--- UBus-react/src/App.js
+++ UBus-react/src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 //pages
 import Home from './pages/Home'
-import SignInPage from './pages/SignIn'
 import SignUpPage from './pages/SignUp'
 import AccountPage from './pages/AccountPage';
 import ActivityPage from './pages/AcitivyPage';
@@ -15,7 +14,6 @@ const App = () => {
         <BrowserRouter>
           <Routes>
             <Route index element={<Home/>}/>
-            <Route path='/sign-in' element={<SignInPage/>}/>
             <Route path='/sign-up' element={<SignUpPage/>}/>
             <Route path='/activity' element={<ActivityPage/>}/>
             <Route path='/account' element={<AccountPage/>}/>
