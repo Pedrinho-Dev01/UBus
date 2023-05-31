372952f5dc0c35809cba1e876f7468713422d906
João Rodrigues <joaofmr@outlook.com>
2023-05-31 21:28:56 +0100

    Fix background image

--- UBus-react/src/pages/SignUpPage.jsx
+++ UBus-react/src/pages/SignUpPage.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import '../App.css'
 import './pages.css'
+import './signup.css'
 
 import { SignUp } from '../containers/';
 
 const SignUpPage = () => {
     return (  
-      <>
-      {/*Page Specific Components*/}
-      <div className='page-content'>
+      <body className='signUpPage'>
+        <>
+        {/*Page Specific Components*/}
+        <div className='page-content'>
           <SignUp/>
-      </div>
-      {/*Page Specific Components*/}
-      </>
+        </div>
+        {/*Page Specific Components*/}
+        </>
+      </body>
     );
   };
 
